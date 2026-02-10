baac5e1512dc70f8988941fbcd629194c81c847f
cyy1156 <3134237155@qq.com>
2026-02-10 21:24:56 +0800

    MFC学习记录

---
+++ Dialog/CDailogExec.cpp
@@ -0,0 +1,34 @@
+﻿// CDailogExec.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "Dialog.h"
+#include "afxdialogex.h"
+#include "CDailogExec.h"
+
+
+// CDailogExec 对话框
+
+IMPLEMENT_DYNAMIC(CDailogExec, CDialogEx)
+
+CDailogExec::CDailogExec(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_EXEC, pParent)
+{
+
+}
+
+CDailogExec::~CDailogExec()
+{
+}
+
+void CDailogExec::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CDailogExec, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CDailogExec 消息处理程序
---
+++ Dialog/CDailogExec.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CDailogExec 对话框
+
+class CDailogExec : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDailogExec)
+
+public:
+	CDailogExec(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CDailogExec();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_EXEC };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
