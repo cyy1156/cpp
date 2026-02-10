1349013b409346b63a46f0ef89db848aaf19931a
cyy1156 <3134237155@qq.com>
2026-02-10 21:21:47 +0800

    MFC学习记录

---
+++ CStaticTest/CStaticTestDlg.cpp
@@ -0,0 +1,191 @@
+﻿
+// CStaticTestDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "CStaticTest.h"
+#include "CStaticTestDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CCStaticTestDlg 对话框
+
+
+
+CCStaticTestDlg::CCStaticTestDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CSTATICTEST_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CCStaticTestDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_TEST, m_test);
+	DDX_Control(pDX, IDC_PICTURE, m_PIC);
+	DDX_Control(pDX, IDC_BUTTON3, m_btn);
+}
+
+BEGIN_MESSAGE_MAP(CCStaticTestDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, &CCStaticTestDlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &CCStaticTestDlg::OnBnClickedButton2)
+	ON_STN_CLICKED(IDC_PICTURE, &CCStaticTestDlg::OnStnClickedPicture)
+	ON_BN_CLICKED(IDC_BUTTON3, &CCStaticTestDlg::OnBnClickedButton3)
+END_MESSAGE_MAP()
+
+
+// CCStaticTestDlg 消息处理程序
+
+BOOL CCStaticTestDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+	
+	//用ststic_picture(用文本显示)显示图片
+
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CCStaticTestDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CCStaticTestDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CCStaticTestDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+//点击按钮
+void CCStaticTestDlg::OnBnClickedButton1()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	m_test.SetWindowTextW(TEXT("呵呵"));
+}
+
+void CCStaticTestDlg::OnBnClickedButton2()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CString str;
+	m_test.GetWindowTextW(str);
+	MessageBox(str);
+}
+
+void CCStaticTestDlg::OnStnClickedPicture()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
+
+void CCStaticTestDlg::OnBnClickedButton3()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	//点击修改显示内容
+	m_btn.EnableWindow(FALSE);
+}
---
+++ CStaticTest/CStaticTestDlg.h
@@ -0,0 +1,46 @@
+﻿
+// CStaticTestDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CCStaticTestDlg 对话框
+class CCStaticTestDlg : public CDialogEx
+{
+// 构造
+public:
+	CCStaticTestDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CSTATICTEST_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+private:
+	CStatic m_test;
+public:
+	afx_msg void OnBnClickedButton2();
+private:
+	CStatic m_PIC;
+public:
+	afx_msg void OnStnClickedPicture();
+	afx_msg void OnBnClickedButton3();
+	CButton m_btn;
+};
