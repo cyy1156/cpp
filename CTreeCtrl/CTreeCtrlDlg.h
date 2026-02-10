62322172d98b0ba317a9a58e398e1b636dc88330
cyy1156 <3134237155@qq.com>
2026-02-10 21:22:45 +0800

    MFC学习记录

---
+++ CTreeCtrl/CTreeCtrlDlg.h
@@ -0,0 +1,38 @@
+﻿
+// CTreeCtrlDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CCTreeCtrlDlg 对话框
+class CCTreeCtrlDlg : public CDialogEx
+{
+// 构造
+public:
+	CCTreeCtrlDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CTREECTRL_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+private:
+	CTreeCtrl m_tree;
+public:
+	afx_msg void OnTvnSelchangedTree1(NMHDR* pNMHDR, LRESULT* pResult);
+};
